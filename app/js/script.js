8cd0cd743206d7cd8825b16e95771e32f647d866
sidahmedabdelillah <s_bentegar@enst.dz>
2021-11-15 17:17:54 +0100

    mm:

--- app/js/script.js
+++ app/js/script.js
@@ -195,3 +195,83 @@ const toggleMenu = () => {
 	const nav = document.getElementById('nav')
 	nav.classList.toggle('nav--open');
 }
+
+const getCode = async  () => {
+  const countryCode = document.getElementById('country_code').value
+  const phoneNumber = document.getElementById('phone_number').value
+  if(countryCode == '' || phoneNumber == '' ){
+    toastr.warning('Please Insert contry code and phone number to get code')
+    return
+  }
+
+  const fd = new FormData();
+  fd.append('country_code' ,countryCode);
+  fd.append('phone_number' , phoneNumber);
+  $('.form-container__button--send').attr('disabled' , false);
+
+  try{
+    const { data } = await axios.post('https://yupa.io/auth.php' , fd , {
+      headers : {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    })
+    toastr.success("An sms was sent to your number")
+  }catch(err){
+    toastr.error("Something went wrong please verify your data")
+  }
+  
+}
+
+const openForm = () => {
+  gsap.to('#formcontainer' , {
+    top: 40
+  })
+}
+
+const sendForm = async () => {
+  const name = $('#name').val();
+  const company = $('#company').val();
+  const email =  $('#email').val();
+  const req = $('#req').val();
+  const code = $('#code').val()
+  const countryCode = document.getElementById('country_code').value
+  const phoneNumber = document.getElementById('phone_number').value
+  if(countryCode == '' || phoneNumber == '' ){
+    toastr.warning('Please Insert contry code and phone number to get code')
+    return
+  }
+  if(!validateForm()) {
+    toastr.warning('Please fill all info in the form')
+    return
+  }
+
+  const fd = new FormData()
+  fd.append('country_code' ,countryCode)
+  fd.append('phone_number' ,phoneNumber)
+  fd.append('code' ,code)
+  fd.append('name' ,name)
+  fd.append('company_name' ,company)
+  fd.append('email' ,email)
+  fd.append('request' ,req)
+  
+  try{
+    const { data } = axios.post('https://yupa.io/verify.php' , fd , {
+      headers : {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      }
+    })
+    toastr.success('You request has been registred we will contact you soon')
+    
+  }catch(err){
+    toastr.error('Something went wrong please verify the validation code and try again')
+
+  }
+
+
+
+}
+
+const validateForm = () => {
+  if($('#name').val() == '' &&  $('#company').val() == '' && $('#email').val() == '' && $('#req').val() == '' && $('#code').val() == ''  ) return false;
+  return true ;
+}
